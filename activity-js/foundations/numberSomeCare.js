11cd28e1f6dba8a1d1672d84d4f3cbbda11d89f7
AlbertoParente <albertoparente20@gmail.com>
2020-03-21 23:55:12 -0300

    Adding numberSomeCare concept

---
+++ activity-js/foundations/numberSomeCare.js
@@ -0,0 +1,7 @@
+console.log(7 / 0.0000000001)
+console.log(7 / 0) // Result Infinity
+console.log("10" / 2)
+console.log("Show" * 2) // Result in error
+console.log(0.1 + 0.7) // Imprecision
+// console.log(10.toString()) // Invalid or unexpected token
+console.log((10.345).toFixed(2))
