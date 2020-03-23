1c1635645a0acf736ca0ca037a8e8d1426cde34b
AlbertoParente <albertoparente20@gmail.com>
2020-03-23 16:16:42 -0300

    Adding null and undefined concept

---
+++ activity-js/foundations/nullAndUndefined.js
@@ -0,0 +1,36 @@
+/*const a = {name: 'Test 1'}
+console.log(a)
+
+const b = a
+b.name = 'Test 2'
+console.log(b)
+
+let c = 3
+let d = c
+d++
+console.log(d)
+console.log(c)
+*/
+
+let value
+console.log(value)
+
+value = null
+console.log(value)
+//console.log(value.toString()) // Erro!
+
+const product = {}
+console.log(product.price)
+console.log(product)
+
+product.price = 3.50
+console.log(product)
+
+product.price = undefined // Avoid assigning undefined
+console.log(!!product.price)
+// delete product.price
+console.log(product)
+
+product.price = null
+console.log(!!product.price)
+console.log(product)
