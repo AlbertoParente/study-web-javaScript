9d932d2f2e27f5a1e37a68586e5e07f8b688234f
AlbertoParente <albertoparente20@gmail.com>
2020-03-25 19:48:30 -0300

    Adding hoisting concept

---
+++ activity-js/foundations/hoisting.js
@@ -0,0 +1,26 @@
+// Using var
+console.log('a =', a)
+var a = 2
+console.log('a =', a)
+
+function test1() {
+    console.log('a =', a)
+    var a = 3
+    console.log('a =', a)
+}
+
+test1()
+
+// Using let
+
+// console.log('b =', b)
+let b = 2
+console.log('b =', b)
+
+function test2() {
+    // console.log('b =', b)
+    let b = 3
+    console.log('b =', b)
+}
+
+test2()
