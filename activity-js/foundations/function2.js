b7cd73dd13c4ca386ecd944e7dc40eb814a0f959
AlbertoParente <albertoparente20@gmail.com>
2020-03-24 23:01:31 -0300

    Adding continuation of the function concept

---
+++ activity-js/foundations/function2.js
@@ -0,0 +1,19 @@
+const printSum = function (a, b) {
+    console.log(a + b)
+}
+
+printSum(2, 3)
+
+const sum = (a, b) => {
+    return a + b
+}
+
+console.log(sum(2, 3))
+
+const subtraction = (a, b) => a - b
+
+console.log(subtraction(3, 2))
+
+const print = a => console.log(a)
+
+print('Hello...')
