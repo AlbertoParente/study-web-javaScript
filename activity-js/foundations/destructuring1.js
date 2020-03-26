b2d96588d1933ae1389916ba6d52e42b8a64de8d
AlbertoParente <albertoparente20@gmail.com>
2020-03-26 20:33:47 -0300

    Adding destructuring concept

---
+++ activity-js/foundations/destructuring1.js
@@ -0,0 +1,20 @@
+const person = {
+    name: 'Alberto',
+    age: 27,
+    adress: {
+        street: 'Rua teste',
+        number: 13
+    }
+}
+
+const { name, age } = person
+console.log(name, age)
+
+const { name: n, age: a } = person
+console.log(n, a)
+
+const { surname,  lastName = true} = person
+console.log(surname, lastName)
+
+const { adress: {street, number, postalCode = true} } = person
+console.log(street, number, postalCode)
