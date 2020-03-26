bf272ae1d3af575a0322d24ccf3507aea0ccce40
AlbertoParente <albertoparente20@gmail.com>
2020-03-26 19:57:31 -0300

    Adding parNameValue concept

---
+++ activity-js/foundations/parNameValue.js
@@ -0,0 +1,22 @@
+const greeting = 'Hello' 
+
+function exec() {
+    const greeting = 'hello...'
+    return greeting
+}
+
+
+const client = {
+    name: 'Alberto',
+    age: 27,
+    weigth: 70,
+    adress: {
+        street: 'Rua teste',
+        number: 13,
+    }
+}
+
+console.log(greeting)
+console.log(exec())
+console.log(client)
+console.log(client.adress)
