fcbe9d90743c50d7470eaa117bed853aeb95a8b0
AlbertoParente <albertoparente20@gmail.com>
2020-03-27 22:42:57 -0300

    Adding arithmetic concept

---
+++ activity-js/foundations/arithmetic.js
@@ -0,0 +1,10 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const sum = a + b + c + d
+const subtraction = d - b
+const multiplication = a * a
+const division = d / a
+const modulee = a % 2
+
+console.log(sum, subtraction, multiplication, division, modulee)
+console.log(-sum, -subtraction, -multiplication, -division, -modulee)
