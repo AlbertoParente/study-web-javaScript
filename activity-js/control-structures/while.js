210cbb2865d0073bf9d86ca3e25ecb6a6a05d5d9
AlbertoParente <albertoparente20@gmail.com>
2020-03-29 22:27:59 -0300

    Adding while concept

---
+++ activity-js/control-structures/while.js
@@ -0,0 +1,13 @@
+function getRandomIntegerBetween(min, max) {
+    const value = Math.random() * (max - min) + min
+    return Math.floor(value)
+}
+
+let option = 0
+
+while(option != -1) {
+    option = getRandomIntegerBetween(-1, 10)
+    console.log(`Chosen option was ${option}.`)
+}
+
+console.log('To the next!')
