20187415b862feb87b300ddff8953795fcbc5ed8
AlbertoParente <albertoparente20@gmail.com>
2020-03-30 20:56:49 -0300

    Adding for concept

---
+++ activity-js/control-structures/for1.js
@@ -0,0 +1,14 @@
+let counter = 1
+while(counter <= 10) {
+    console.log(`Counter = ${counter}`)
+    counter++
+}
+
+for(let i = 0; i <= 10; i++) {
+    console.log(`Counter = ${i}`)
+}
+
+const note = [6.7, 7.4, 9.8, 8.1, 7.7]
+for(let i = 0; i < note.length; i++) {
+    console.log(`Note = ${note[i]}`)
+}
