5d37bef9d7ea9ff4e4009803545a4a1b8e237840
AlbertoParente <albertoparente20@gmail.com>
2020-03-30 21:08:49 -0300

    Adding for concept, continuation

---
+++ activity-js/control-structures/for2.js
@@ -0,0 +1,16 @@
+const note = [6.7, 7.4, 9.8, 8.1, 7.7]
+
+for(let i in note) {
+    console.log(i, note[i])
+}
+
+const person = {
+    name: 'Alberto',
+    surname: 'Silva',
+    age: 27,
+    weight: 70
+}
+
+for(let attribute in person) {
+    console.log(`${attribute} = ${person[attribute]}`)
+}
