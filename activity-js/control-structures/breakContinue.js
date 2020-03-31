702b979b8618a66682cf6a35f89f7935847a684b
AlbertoParente <albertoparente20@gmail.com>
2020-03-30 21:43:48 -0300

    Adding breakContinue concept

---
+++ activity-js/control-structures/breakContinue.js
@@ -0,0 +1,23 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for(x in numbers) {
+    if(x == 5) {
+        break
+    }
+    console.log(`${x} = ${numbers[x]}`)
+}
+
+for(y in numbers) {
+    if(y == 5) {
+        continue
+    }
+    console.log(`${y} = ${numbers[y]}`)
+}
+
+external:
+for(a in numbers) {
+    for(b in numbers) {
+        if(a == 2 && b == 3) break external
+        console.log(`Par = ${a}, ${b}`)
+    }
+}
