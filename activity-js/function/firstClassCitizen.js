2105d644b06ab872c332f069b1bfb9b34e626c01
AlbertoParente <albertoparente20@gmail.com>
2020-04-01 22:30:02 -0300

    Adding comenting on firstClassCitizen

--- activity-js/function/firstClassCitizen.js
+++ activity-js/function/firstClassCitizen.js
@@ -1,24 +1,33 @@
+// Function in JS is First-Class Object (Citizens)
+// Higher-order function
+
+// Literal form
 function fun1() {
 
 }
 
-function fun2() {
+// In variable
+const fun2 = function() {
     
 }
 
+// In array
 const array = [function (a,b) { return a + b }, fun1, fun2]
 console.log(array[0](2,3))
 
+// In attribute of object
 const obj = {}
 obj.speak = function() { return 'hello' }
 console.log(obj.speak())
 
+// Passing as parameter
 function run(fun){
     fun()
 }
 
 run(function() { console.log('Running...') })
 
+// Return or contain a function
 function sum(a, b) {
     return function (c) {
         console.log(a + b + c)
