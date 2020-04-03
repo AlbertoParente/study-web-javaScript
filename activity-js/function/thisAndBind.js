71cfd11a60e7d8a543fa3f250a6d663bc87fbdde
AlbertoParente <albertoparente20@gmail.com>
2020-04-02 22:13:42 -0300

    Adding thisAndBind concept

---
+++ activity-js/function/thisAndBind.js
@@ -0,0 +1,13 @@
+const person = {
+    greeting: 'Good Morning!',
+    speak() {
+        console.log(this.greeting)
+    }
+}
+
+person.speak()
+const speak = person.speak
+speak() // Conflict between paradigms: functional and OO
+
+const speakOfPerson = person.speak.bind(person)
+speakOfPerson()
