45f48d739840f5c5302cfbb028fee12954499530
AlbertoParente <albertoparente20@gmail.com>
2020-04-02 22:47:33 -0300

    Adding arrow concept, continuation 2

---
+++ activity-js/function/arrowFunction3.js
@@ -0,0 +1,21 @@
+let compareWithThis = function(param) {
+    console.log(this === param)
+}
+
+compareWithThis(global)
+
+const obj = {
+
+}
+
+compareWithThis = compareWithThis.bind(obj)
+compareWithThis(global)
+compareWithThis(obj)
+
+let compareWithThis = parm => console.log(this === param)
+compareWithThis(global)
+compareWithThis(module.exports)
+
+compareWithThis = compareWithThis.bind(obj)
+compareWithThis(global)
+compareWithThis(module.exports)
