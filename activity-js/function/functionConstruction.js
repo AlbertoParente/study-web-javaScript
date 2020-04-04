3fb9046682d769ab00cdb923dbad83df0ac71975
AlbertoParente <albertoparente20@gmail.com>
2020-04-03 23:27:48 -0300

    Adding functionConstruction concept

---
+++ activity-js/function/functionConstruction.js
@@ -0,0 +1,28 @@
+function Car(maximumSpeed = 200, delta = 5) {
+    let maximumSpeed = 0
+
+    this.sppedUp = function() {
+        if(currentSpeed + delta <= maximumSpeed) {
+            currentSpeed += delta
+        } else {
+            currentSpeed = maximumSpeed
+        }
+    }
+
+    this.getCurrentSpeed = function() {
+        return currentSpeed
+    }
+}
+
+const uno = new Car
+uno.sppedUp()
+console.log(uno.getCurrentSpeed())
+
+const ferrari = new Car(350, 20)
+ferrari.sppedUp()
+ferrari.sppedUp()
+ferrari.sppedUp()
+console.log(uno.getCurrentSpeed())
+
+console.log(typeof Car)
+console.log(typeof ferrari)
