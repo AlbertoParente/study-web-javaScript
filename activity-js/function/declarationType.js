663209dd26e959338208431f2e69c06bd979c271
AlbertoParente <albertoparente20@gmail.com>
2020-04-08 23:42:11 -0300

    Adding concept of declaration type

---
+++ activity-js/function/declarationType.js
@@ -0,0 +1,16 @@
+console.log(sum(3,4))
+
+function sum(x, y) {
+    return x + y
+}
+
+cont subtraction = function (x, y) {
+    return x - y
+}
+console.log(subtraction(3,4))
+
+const multiplication = function multiplication(x ,y) {
+    return x * y
+}
+console.log(multiplication(3,4))
+
