a030f59512545d52c238e40ae454c042ca8299f7
AlbertoParente <albertoparente20@gmail.com>
2020-04-09 21:23:26 -0300

    Adding coment in declarationType

--- activity-js/function/declarationType.js
+++ activity-js/function/declarationType.js
@@ -1,14 +1,17 @@
 console.log(sum(3,4))
 
+// Function declaration
 function sum(x, y) {
     return x + y
 }
 
-cont subtraction = function (x, y) {
+// Function expression
+const subtraction = function (x, y) {
     return x - y
 }
 console.log(subtraction(3,4))
 
+// Named function expression
 const multiplication = function multiplication(x ,y) {
     return x * y
 }
