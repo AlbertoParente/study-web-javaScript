b9bb6b882bfd5b5bc74b52bb12ab1c4afd066cd7
AlbertoParente <albertoparente20@gmail.com>
2020-04-20 23:18:07 -0300

    Adding importantFunctions concept

---
+++ activity-js/object/importantFunctions.js
@@ -0,0 +1,33 @@
+const person = {
+    name: 'Juliana',
+    age: 17,
+    weight: 63
+}
+
+console.log(Object.keys(person))
+console.log(Object.values(person))
+console.log(Object.entries(person))
+
+Object.entries(person).forEach(([key, value]) => {
+    console.log(`${e[0]}: ${e[1]}`)    
+})
+
+Object.defineProperties(person, 'dateOfBirth', {
+    enumerable: true,
+    writable: false,
+    value: '2020/01/01'
+})
+
+person.dateOfBirth = '1998/01/01'
+console.log(person.dateOfBirth)
+console.log(Object.keys(person))
+
+// Object.assign (ECMAScript 2015)
+const dest = { a:1 }
+const o1 = { b: 2 }
+const o2 = { c:3, a:4 }
+const obj = Object.assign(dest, o1, o2)
+
+Object.freeze(obj)
+obj.c = 12345
+console.log(obj)
