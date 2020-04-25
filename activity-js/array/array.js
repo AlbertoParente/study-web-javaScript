884f0352f99afa042aa8f3df6b24008fc2460cea
AlbertoParente <albertoparente20@gmail.com>
2020-04-25 19:45:03 -0300

    Adding Array concept

---
+++ activity-js/array/array.js
@@ -0,0 +1,30 @@
+console.log(typeof Array, typeof new Array, typeof [])
+
+let approved = new Array('Juliana', 'Alberto', 'Vitoria')
+console.log(approved)
+
+approved = ['Juliana', 'Alberto', 'Vitoria']
+console.log(approved[0])
+console.log(approved[1])
+console.log(approved[2])
+console.log(approved[3])
+
+approved[3] = 'Alberto Parente'
+approved.push('Julia')
+console.log(approved.length)
+
+approved[9] = 'Dennys'
+console.log(approved.length)
+console.log(approved[8] === undefined)
+
+console.log(approved)
+approved.sort()
+console.log(approved)
+
+delete approved[1]
+console.log(approved[1])
+console.log(approved[2])
+
+approved = ['Juliana', 'Alberto', 'Vitoria']
+approved.splice(1, 1)
+console.log(approved)
