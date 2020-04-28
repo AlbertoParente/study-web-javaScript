0786e57c14d3a6a2cd01da9d1358b273a6cdde84
AlbertoParente <albertoparente20@gmail.com>
2020-04-28 20:37:44 -0300

    Adding forEach concept

---
+++ activity-js/array/forEach1.js
@@ -0,0 +1,10 @@
+const approveds = [ 'Juliana', 'Alberto', 'Julia', 'Graziella' ]
+
+approveds.forEach(function(name, index) {
+    console.log(`${index + 1}, ${name}`)
+})
+
+approveds.forEach(name => console.log(name))
+
+const printApproveds = approveds => console.log(approved)
+approveds.forEach(printApproveds)
