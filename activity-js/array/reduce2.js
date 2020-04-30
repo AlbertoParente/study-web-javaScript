de66ab7e85b5e850c00672d40dab646df4c493ce
AlbertoParente <albertoparente20@gmail.com>
2020-04-30 19:39:51 -0300

    Adding reduce concept, continuation

---
+++ activity-js/array/reduce2.js
@@ -0,0 +1,12 @@
+const students = [
+    { name: 'Alberto', note: 7.3, scholarship: false },
+    { name: 'Juliana', note: 9.2, scholarship: true },
+    { name: 'Graziella', note: 9.8, scholarship: false },
+    { name: 'Gabriella', note: 8.7, scholarship: true }
+]
+
+const allScholarship =(result, scholarship) => result && scholarship
+console.log(students.map(a => a.scholarship).reduce(allScholarship))
+
+const someScholarship =(result, scholarship) => result || scholarship
+console.log(students.map(a => a.scholarship).reduce(someScholarship))
