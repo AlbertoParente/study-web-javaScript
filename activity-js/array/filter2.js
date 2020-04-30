0999e17cfe0d012852e1a82746369721ca1ac045
AlbertoParente <albertoparente20@gmail.com>
2020-04-30 19:26:02 -0300

    Adding filter concept, continuation

---
+++ activity-js/array/filter2.js
@@ -0,0 +1,21 @@
+Array.prototype.filter2 = function(callback) {
+    const newArray = []
+    for(let i =0; i < this.length; i++) {
+        if(callback(this[i], i, this)) {
+            newArray.push(this[i])
+        }
+    }
+    return newArray
+}
+
+const product = [
+    { name: 'Notebook', price: 2499, fragile: true },
+    { name: 'Ipad Pro', price: 4199, fragile: true },
+    { name: 'Glass', price: 12.49, fragile: true },
+    { name: 'Plastic Cup', price: 18.99, fragile: true }
+]
+
+const car = product => product.price >= 500
+const fragile = product => product.fragile
+
+console.log(product.filter2(car).filter2(fragile))
