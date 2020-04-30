772f3cbdc8d99c8ad58dedd7f03c32439c573999
AlbertoParente <albertoparente20@gmail.com>
2020-04-29 21:33:49 -0300

    Adding map concept, continuation

---
+++ activity-js/array/map2.js
@@ -0,0 +1,14 @@
+const cart = [
+    '{ "name": "Rubber", "price": 3.45 }',
+    '{ "name": "Book", "price": 13.90 }',
+    '{ "name": "Pencil Kit ", "price": 41.22 }',
+    '{ "name": "Pen", "price": 7.50 }'
+]
+
+// Return a Array wiht prices
+
+const forObject = json => JSON.parse(json)
+const onlyPrice = product => product.price
+
+const result = cart.map(forObject).map(onlyPrice)
+console.log(result)
