1316e5ce7c68de2417c530d88f97de3ac0a7b78c
AlbertoParente <albertoparente20@gmail.com>
2020-04-30 20:01:53 -0300

    Adding concat concept

---
+++ activity-js/array/concat.js
@@ -0,0 +1,6 @@
+const girls = ['Ualeskah', 'Cibalena']
+const boys = ['Uoxiton', 'Uesclet']
+const all = girls.concat(boys)
+console.log(all, girls, boys)
+
+console.log([].concat([1, 2], [3, 4], 5,  [[6, 7]]))
