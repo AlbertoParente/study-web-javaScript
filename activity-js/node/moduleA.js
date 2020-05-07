a647dad6072c85148ca3faebe5c1612bfda2c8ca
AlbertoParente <albertoparente20@gmail.com>
2020-05-06 23:28:18 -0300

    Adding node concept

---
+++ activity-js/node/moduleA.js
@@ -0,0 +1,5 @@
+console.log(this)
+
+this.hello = 'Hello'
+exports.welcome = 'Welcome to Node...!'
+module.exports.bye = 'Bye...!'
---
+++ activity-js/node/moduleB.js
@@ -0,0 +1,8 @@
+let a = 2
+
+module.exports = {
+    goodMorning: 'GoodMorning...!',
+    goodNight() {
+        return 'Good Night...!'
+    }
+}
