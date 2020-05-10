c47737eedd6fd657d817e829195c6dafc2d5780d
AlbertoParente <albertoparente20@gmail.com>
2020-05-09 21:30:46 -0300

    Implementing a method to search for an employee with the lowest salary in a json file

--- activity-js/node/employees/employees.js
+++ activity-js/node/employees/employees.js
@@ -1,7 +1,20 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
+const chinese = f => f.pais === 'China'
+const women = f => f.genero === 'F'
+const lowestSalary = (employee, employeeActual) => {
+    return employee.salario < employeeActual.salario ? employee : employeeActual
+}
+
 axios.get(url).then(response => {
     const employees = response.data
-    console.log(employees)
+    // console.log(employees)
+
+    const employee = employees
+        .filter(chinese)
+        .filter(women)
+        .reduce(lowestSalary)
+
+    console.log(employee)
 })
