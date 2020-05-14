95eee56ad442fe56b619d5b94019721a70f24c24
AlbertoParente <albertoparente20@gmail.com>
2020-05-13 22:49:33 -0300

    Creating CRUD

--- activity-js/node/project/src/server.js
+++ activity-js/node/project/src/server.js
@@ -2,9 +2,22 @@ const port = 3003
 
 const express = require('express')
 const app = express()
+const database = require('./database')
 
 app.get('/products', (req, res, next) => {
-    res.send({ name: 'Notebook', price: 123.45 }) // convert for JSON
+    res.send(database.getProductsAll())
+})
+
+app.get('/products/:id', (req, res, next) => {
+    res.send(database.getProductId(req.params.id))
+})
+
+app.post('/products', (req, res, next) => {
+    const product = database.saveProduct({
+        name: req.body.name,
+        price: req.body.price
+    })
+    res.send(product) // Convert in JSON
 })
 
 app.listen(port, () => {
