c2e3641f41dcf109a8f70611525c1a212f4b39f3
AlbertoParente <albertoparente20@gmail.com>
2020-05-14 19:39:26 -0300

    Adding review 2

---
+++ activity-js/esNext/review2.js
@@ -0,0 +1,24 @@
+// Arrow Function
+const sum = (a, b) => a + b
+console.log(sum(2, 3))
+
+// Arrow Function (this)
+const lexical1 = () => console.log(this === exports)
+const lexical2 = lexical1.bind({})
+lexical1()
+lexical2()
+
+// Parameters default
+function log(text = 'Node') {
+    console.log(text)
+}
+log()
+log('I am stronger...!')
+
+// Operator rest
+function total(...numbers) {
+    let total = 0
+    numbers.forEach(element => total += element)
+    return total
+}
+console.log(total(2 , 3, 4, 5))
