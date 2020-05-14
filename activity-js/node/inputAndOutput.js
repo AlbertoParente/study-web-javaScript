dd6148962f828165b9f4ff0f0c547e376fbf02ba
AlbertoParente <albertoparente20@gmail.com>
2020-05-14 00:00:06 -0300

    Adding concept input and output

---
+++ activity-js/node/inputAndOutput.js
@@ -0,0 +1,15 @@
+const anonymous = process.argv.indexOf('-a') !== -1
+// console.log(anonymous)
+
+if(anonymous) {
+    process.stdout.write('Speak anonymous...!\n')
+    process.exit()
+} else {
+    process.stdout.write('Enter your name: ')
+    process.stdin.on('data', data => {
+        const name = data.toString().replace('\n', '')
+
+        process.stdout.write(`Speak ${name}!!\n`)
+        process.exit()
+    })
+}
