398368b1c9117c97917bf372bbda9d710c41085c
AlbertoParente <albertoparente20@gmail.com>
2020-05-13 23:37:37 -0300

    Adding temorizing concept

---
+++ activity-js/node/temporizing.js
@@ -0,0 +1,22 @@
+const schedule = require('node-schedule')
+
+const task1 = schedule.scheduleJob('*/5 * 23 * * 3', function() {
+    console.log('Running Task 1!', new Date().getSeconds())
+})
+
+setTimeout(function() {
+    task1.cancel()
+        console.log('Canceling Task 1!')
+}, 20000)
+
+const rule = new schedule.RecurrenceRule()
+rule.day0fWeek = [new schedule.Range(1, 5)]
+rule.hour = 23
+rule.second = 30
+
+const task2 = schedule.scheduleJob(rule, function() {
+    console.log('Running Task 2!', new Date().getSeconds())
+})
+
+// setImmediate
+//setInterval
