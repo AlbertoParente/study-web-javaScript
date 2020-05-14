d8c16017be90d591456d4e03df20743cf0ff842f
AlbertoParente <albertoparente20@gmail.com>
2020-05-14 19:49:53 -0300

    Adding review 3

---
+++ activity-js/esNext/review3.js
@@ -0,0 +1,23 @@
+// ES8: Object.values / Object.entries
+const obj = { a: 1, b: 2, c: 3 }
+console.log(Object.values(obj))
+console.log(Object.entries(obj))
+
+// Literal notation
+const name = 'Alberto'
+const person = {
+    name,
+    hello() {
+        return 'Hello person...!'
+    }
+}
+console.log(person.name, person.hello())
+
+// Class
+class Animal {}
+class Dog extends Animal {
+    speak() {
+        return 'Au Au!'
+    }
+}
+console.log(new Dog().speak())
