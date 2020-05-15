6034614d86364b9f5202fdd3081cdde867ac705a
AlbertoParente <albertoparente20@gmail.com>
2020-05-15 17:40:48 -0300

    Adding async/Await concept

---
+++ activity-js/esNext/asyncAwait.js
@@ -0,0 +1,34 @@
+const http = require('http')
+
+const getGroup = letter => {
+    const url = `http://files.cod3r.com.br/curso-js/turma${letter}.json`
+    return new Promise((resolve, reject) => {
+        http.get(url, res => {
+            let result = ''
+    
+            res.on('data', dados => {
+                result += dados
+            })
+    
+            res.on('end', () => {
+                try {
+                    resolve(JSON.parse(result))
+                } catch(e) {
+                    reject(e)
+                }
+            })
+        })
+    })
+}
+
+let getStudents = async () => {
+    const groupA = await getGroup('A')
+    const groupB = await getGroup('B')
+    const groupC = await getGroup('C')
+    return [].concat(groupA, groupB, groupC)
+}
+
+getStudents()
+    .then(studensts => studensts.map(student => student.nome))
+    .then(names => console.log(names))
+    .catch(e => console.log(e.message))
