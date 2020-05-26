5a8877e49e114e82cd7ee8961efa6b9577109ecf
AlbertoParente <albertoparente20@gmail.com>
2020-05-26 13:00:47 -0300

    Adding form concept, continuation

--- activity-js/html/activity/form/server.js
+++ activity-js/html/activity/form/server.js
@@ -6,7 +6,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/users', (req, resp) => {
     console.log(req.body)
-    resp.send('<h1>Success</h1>')
+    resp.send('<h1>Successfully Created...!</h1>')
+})
+
+app.post('/users/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Successfully Edited...!</h1>')
 })
 
 app.listen(3003)
