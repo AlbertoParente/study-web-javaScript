bbe1fb10c390943894db5cc459a9702766970a0d
AlbertoParente <albertoparente20@gmail.com>
2020-06-06 22:17:22 -0300

    Adding matrix concept

---
+++ activity-js/array/matrix.js
@@ -0,0 +1,13 @@
+function Person(firstName, lastName) {
+    this.firstName = firstName
+    this.lastName = lastName
+}
+
+var me = new Person('John', 'Smith','Alberto')
+
+console.table(me)
+
+// with array of arrays
+
+var people = [['John', 'Smith'], ['Jane', 'Doe']]
+console.table(people)
