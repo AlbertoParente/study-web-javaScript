0fca79bc527e4788f758dafc4b07666466752956
AlbertoParente <albertoparente20@gmail.com>
2020-08-07 00:08:52 -0300

    Creating method for create Barrier in flappy Bird project

--- activity-js/dom/js/flappy.js
+++ activity-js/dom/js/flappy.js
@@ -1,1 +1,20 @@
-// Code....
+function newElement(tagName, className) {
+    const e = document.createElement(tagName)
+    e.className = className
+    return e
+}
+
+function Barrier(reverse = false) {
+    this.element = newElement('div', 'barrier')
+
+    const borderBarrier = newElement('div', 'border')
+    const bodyBarrier = newElement('div', 'body')
+    this.element.appendChild(reverse ? bodyBarrier : borderBarrier)
+    this.element.appendChild(reverse ? borderBarrier : bodyBarrier)
+
+    this.setHeightBarrier = heightBarrier => bodyBarrier.style.height = `${heightBarrier}px`
+}
+
+const b = new Barrier(true)
+b.setHeightBarrier(300)
+document.querySelector('[attr-flappy]').appendChild(b.element)
