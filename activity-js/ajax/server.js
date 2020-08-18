9b5c62e5ca573c6b96704632118e84b09ce0d4d6
AlbertoParente <albertoparente20@gmail.com>
2020-08-17 22:33:44 -0300

    Adding XMLHTTPRequest concept

--- activity-js/ajax/server.js
+++ activity-js/ajax/server.js
@@ -6,5 +6,5 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.get('/test', (req, res) => res.send('Ok'))
+// app.get('/test', (req, res) => res.send('Ok'))
 app.listen(8080, () => console.log('Server is running...'))
