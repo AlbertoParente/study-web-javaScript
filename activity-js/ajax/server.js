8c520ec30cb8b0a7beffb41553805bf800b08feb
AlbertoParente <albertoparente20@gmail.com>
2020-08-19 22:06:36 -0300

    Adding XMLHTTPRequest concept, continuation (Creating file uploads)

--- activity-js/ajax/server.js
+++ activity-js/ajax/server.js
@@ -1,10 +1,31 @@
 const bodyParser = require('body-parser')
 const express = require('express')
+const multer = require('multer')
 const app = express()
 
 app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+const storage = multer.diskStorage({
+    destination: function(req, res, callback) {
+        callback(null, '.')
+    },
+    filename: function(req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('archive')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if(err) {
+            return res.end('Error occurred...')
+        }
+        res.end('Completed...')
+    })
+})
 
 // app.get('/test', (req, res) => res.send('Ok'))
 app.listen(8080, () => console.log('Server is running...'))
