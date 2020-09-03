dbc2e24dea92f2739086bdc8259376a506a53f16
AlbertoParente <albertoparente20@gmail.com>
2020-09-02 23:33:52 -0300

    Adding gulp concept, continuation

--- activity-js/gulp/basic/gulpfile.js
+++ activity-js/gulp/basic/gulpfile.js
@@ -1,9 +1,36 @@
 const gulp = require('gulp')
 const { series } = require('gulp')
 
-function copy(cb) {
-    console.log('Task copy!')
+const firstTask = cb => {
+    console.log('Task 1!')
     return cb()
 }
 
-module.exports.default = series(copy)
+const secondTask = cb => {
+    console.log('Task 2!')
+    return cb()
+}
+
+function copy(cb) {
+    // gulp.src(['folder/file1.txt, folder/file2.txt'])
+    gulp.src('folder/**/*.txt')
+        // .pipe(halfImage())
+        // .pipe(blackAndWhiteImage())
+        // .pipe(transformationA())
+        // .pipe(transformationB())
+        // .pipe(transformationC())
+        .pipe(gulp.dest('folderB'))
+    return cb()
+}
+
+
+const end = cb => {
+    console.log('Task end 1!')
+    return cb()
+}
+
+module.exports.default = series(
+    parallel(firstTask, secondTask),
+    copy,
+    end
+)
