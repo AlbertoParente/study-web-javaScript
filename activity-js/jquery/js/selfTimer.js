31a392045f6ff4e50931c96a864a1e6df7c75e04
AlbertoParente <albertoparente20@gmail.com>
2020-10-13 23:22:16 -0300

    Adding plugin in Jquery concept, continuation

--- activity-js/jquery/js/selfTimer.js
+++ activity-js/jquery/js/selfTimer.js
@@ -4,6 +4,20 @@
             message: 'Coming soon!!!',
             time: '23:59:59'
         }, options)
+
+        const timeTens = $('<apan class="digit">').html('0')
+        const hourUnit = $('<apan class="digit">').html('0')
+        const minuteTens = $('<apan class="digit">').html('0')
+        const minuteUnit = $('<apan class="digit">').html('0')
+        const secondTens = $('<apan class="digit">').html('0')
+        const secondUnit = $('<apan class="digit">').html('0')
+
+        const separatorHour = $('<apan class="separator">').html(':')
+        const separatorUnit = $('<apan class="digit">').html(':')
+
+        const menssage = $('<apan class="menssage">').html(options.menssage)
+
+        $(this).addClass('selfTimer')
         return this
     }
-})
+})(jQuery)
