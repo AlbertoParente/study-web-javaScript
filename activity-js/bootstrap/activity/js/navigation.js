01ca13e15b76941769df80e2c7f3fe05a6ff8774
AlbertoParente <albertoparente20@gmail.com>
2020-11-18 22:31:03 -0300

    Adding tooltip in bootstrap concept

--- activity-js/bootstrap/activity/js/navigation.js
+++ activity-js/bootstrap/activity/js/navigation.js
@@ -12,6 +12,7 @@
             .then(resp => resp.text())
             .then(html => {
                 destin.innerHTML = html
+                eval(html.match(/\<script\>([\s\S]*)\<\/script\>/)[1])
             })
     }
 
