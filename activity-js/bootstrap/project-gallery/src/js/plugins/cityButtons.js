4845ef29e412fe246edd81784d4770cf130d8b98
AlbertoParente <albertoparente20@gmail.com>
2020-11-30 23:22:52 -0300

    Filtering photos by city

--- activity-js/bootstrap/project-gallery/src/js/plugins/cityButtons.js
+++ activity-js/bootstrap/project-gallery/src/js/plugins/cityButtons.js
@@ -30,3 +30,24 @@ const btnGroup = $('<div>').addClass(['btn-group'])
 btnGroup.append(btns)
 
 cityButtons.html(btnGroup)
+
+
+import $ from 'jquery'
+
+function loadIncludes(parent) {
+    if(!parent) parent = 'body'
+    $(parent).find('[attr-include]').each(function(i, e) {
+        const url = $(e).attr('attr-include')
+        $.ajax({
+            url,
+            success(data) {
+                $(e).html(data)
+                $(e).removeAttr('attr-include')
+
+                loadIncludes(e)
+            }
+        })
+    })
+}
+
+loadIncludes()
